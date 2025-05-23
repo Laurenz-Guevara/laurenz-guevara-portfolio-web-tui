55a674567415f3ab84a4c90dde0fb024e49950af
Laurenz Guevara <laurenzguevara@outlook.com>
2025-05-23 23:08:19 +0100

    feat: Remove skill section from navigation bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,7 @@ export default function Navigation() {
     <div className="flex bg-surface-0/50 border-y divide-x-2 divide-surface-1 border-surface-1 text-sm">
       <Link className="py-2 px-4" href="/">Home</Link>
       <Link className="py-2 px-4" href="/experience">Experience</Link>
-      <Link className="py-2 px-4" href="/skills">Skills</Link>
+      {/* <Link className="py-2 px-4" href="/skills">Skills</Link> */}
       <Link className="py-2 px-4" href="/blog">Blog</Link>
       <Link className="py-2 px-4 border-r border-surface-1" href="/contact">
         Contact
