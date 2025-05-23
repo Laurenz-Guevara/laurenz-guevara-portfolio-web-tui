aa6a27064ef9308990eaff0c40b995ad72071edb
Laurenz Guevara <laurenzguevara@outlook.com>
2025-05-23 13:35:04 +0100

    feat: Added routes

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,3 @@
+export default function Experience() {
+  return <h1>Hello Experience</h1>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Navigation from "@/components/Navigation";
 
 export const metadata: Metadata = {
   title: "Laurenz Guevara",
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         <Header />
+        <Navigation />
         {children}
         <Footer />
       </body>
